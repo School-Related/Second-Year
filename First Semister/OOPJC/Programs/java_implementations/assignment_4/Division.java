f3c44436a9605519413e90af6644c92677cb004a
KrishnarajT <krishnaraj.kpt@outlook.com>
2022-09-30 15:12:47 +0530

    daily commit

---
+++ First Semister/OOPJC/Programs/java_implementations/assignment_4/Division.java
@@ -0,0 +1,49 @@
+package assignment_4;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+// Implement the program to handle the arithmetic exception, ArrayIndexOutofBounds .
+// The user enters the two numbers: n1, n2. The division of n1 and n2 is displayed. If n1, n2
+// are not integers then program will throw number format exception. If n2 is zero the
+// program will throw Arithmetic exception.
+
+// class NumberFormatException extends Exception
+// {
+//     public NumberFormatException(String s)
+//     {
+//         super(s);
+//     }
+
+//     @Override
+//     public String getMessage()
+//     {
+//         return "What is this";
+//     }
+// }  
+
+public class Division {
+    int n1;
+    int n2;
+    int ans;
+    Scanner input = new Scanner(System.in);
+
+    void accept() {
+        try {
+            n1 = input.nextInt();
+            n2 = input.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Number Format Exception, the format you entered does not match. ");
+        }
+    }
+
+    int divide() {
+        try {
+            ans = n1 / n2;
+            return ans;
+        } catch (ArithmeticException e) {
+            System.out.println("Exception Caught! Cant divide by Zero!");
+        }
+    }
+
+}
--- First Semister/OOPJC/Programs/java_implementations/assignment_4/Main.java
+++ First Semister/OOPJC/Programs/java_implementations/assignment_4/Main.java
@@ -1,9 +1,34 @@
 package assignment_4;
 
 public class Main {
-    public static void main(String args[]) {
+
+    public static void program_3() {
         Bank obj = new Bank();
         obj.accept();
         obj.display();
+    }
+
+    public static void program_2() {
+        Division d = new Division();
+        d.accept();
+        System.out.printn("Dividing the inputs: ");
+        System.out.println(d.divide());
+
+    }
+
+    public static void program_1() {
+
+        Employee obj = new Employee();
+        if (obj.accept() >= 0) {
+            obj.display();
+        }
+    }
+
+    public static void main(String args[]) {
+
+        program_1();
+        program_2();
+        program_3();
+
     }
 }
