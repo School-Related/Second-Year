101556e9138c5445fea9894c6c85c6e2e60d147c
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-03-15 06:04:42 +0000

    lab

--- Second Semester/Advanced Data Structures/Programs/Assignment_5.cpp
+++ Second Semester/Advanced Data Structures/Programs/Assignment_5.cpp
@@ -1,3 +1,58 @@
 // Creating of a Network in a Graph
+#include <iostream>
 
+using namespace std;
 
+class GraphNode
+{
+    int vertex;
+    GraphNode *next;
+    friend class Graph;
+};
+
+class Graph
+{
+private:
+    GraphNode *head[20];
+    int no_of_vtex;
+    char choice;
+
+public:
+    Graph()
+    {
+        for (int i = 0; i < no_of_vtex; i++)
+        {
+            head[i] = new GraphNode;
+            head[i]->vertex = i;
+        }
+    }
+    void create_graph()
+    {
+        int current_vertex;
+        int choice;
+        for (int i = 0; i < no_of_vtex; i++)
+        {
+            temp = head[i];
+            do
+            {
+                cin >> current_vertex;
+                if (current_vertex == i)
+                {
+                    cout << "Self Loops are not allowed" << endl;
+                }
+                else
+                {
+                    GraphNode *current = new GraphNode;
+                    current->vertex = v;
+                    temp->next = current;
+                    temp = temp->next;
+                }
+                cin >> choice;
+            } while (choice == 'y' || choice == 'Y');
+        }
+    }
+};
+int main()
+{
+    return 0;
+}
