f1dded730bcbaa68c946142da2ab5e0f8a5d1ba6
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-03-21 12:10:46 +0530

    done some python stuff

--- Second Semester/Advanced Data Structures/Programs/Random Programs/random_hash.cpp
+++ Second Semester/Advanced Data Structures/Programs/Random Programs/random_hash.cpp
@@ -2,4 +2,34 @@
 
 using namespace std;
 
-int insert_
+int insert_linear_probing_without_replacement(int arr[], int size, int key)
+{
+    int i = key % size;
+    int j = i;
+    do
+    {
+        if (arr[i] == -1)
+        {
+            arr[i] = key;
+            return i;
+        }
+        else
+        {
+            i = (i + 1) % size;
+        }
+    } while (i != j);
+    return -1;
+}
+
+int main(){
+    int arr[10];
+    for (int i = 0; i < 10; i++)
+    {
+        arr[i] = -1;
+    }
+    int key;
+    cin >> key;
+    int index = insert_linear_probing_without_replacement(arr, 10, key);
+    cout << index << endl;
+    return 0;
+}
