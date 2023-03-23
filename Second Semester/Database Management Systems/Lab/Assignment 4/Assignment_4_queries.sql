fe1a5cc94ba1cf7826687e17b5692406cc421f1b
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-03-23 12:40:43 +0530

    added some queries for assignment 5 and 4

--- Second Semester/Database Management Systems/Lab/Assignment 4/Assignment_4_queries.sql
+++ Second Semester/Database Management Systems/Lab/Assignment 4/Assignment_4_queries.sql
@@ -11,21 +11,57 @@ select count(*) from Hotel;
 
 -- 2. the price and type of all rooms at the Grosvenor Hotel.
 select price, type, Name from Room, Hotel where Room.HotelNo = Hotel.HotelNo and Name = 'Hotel love';
-select count(RoomNo) from (select * from Room INNER JOIN Hotel where Hotel.HotelNo = Room.HotelNo);
-
-select RoomNo, Hotel.HotelNo from Room INNER JOIN Hotel where Hotel.HotelNo = Room.HotelNo;
 
 -- 3. the number of rooms in each hotel.
+select Room.HotelNo, Hotel.NAME,  count(*) from Room, Hotel where Room.HotelNo = Hotel.HotelNo group by HotelNo;
 
--- 4. the price of all rooms by 5%.
+-- 4. Update the price of all rooms by 5%.
+select r.Price, r.Price + r.Price * 0.05 as Updated_price from Room r;
 
 -- 5. full details of all hotels in London.
 
--- 6. is the average price of a room?
+select * from Hotel where City = 'Jordan';
+
+-- 6. What is the average price of a room?
+
+select avg(Price) from Room;
+
 
 -- 7. all guests currently staying at the Grosvenor Hotel.
 
+select Guest.* from Guest, booking, Hotel where Guest.GuestNo = booking.GuestNo and booking.HotelNo = Hotel.HotelNo and Hotel.Name = 'Hotel pool';
+
 -- 8. the number of rooms in each hotel in London.
+
+select count(*) from Room, Hotel where Room.HotelNo = Hotel.HotelNo and Hotel.City = 'Jordan';
 
 -- 9. one view on above database and query it.
 
+create view IF NOT EXISTS HotelView as select Hotel.
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- emp (eno,ename,Zip,hdate)
+-- – parts(pno,pname,qty_on_hand, price)
+-- – customer(cno,cname,street,Zip,phone)
+-- – order(ono,cno,receivedate,shippeddate)
+-- – odetails(ono,pno,qty)
+-- – zipcode(Zip,city)
+-- Solve the following queries
+-- get pno,pname values of parts that are priced less than $20.00
+-- get the ono & cname values of customer whose orders have not yet been shipped
+-- get the cname values of customer who have placed order with emp living in ‘Pune’ or ‘mumbai’
+-- get the cities in which customer or emp are located
+-- get the totalquantity of part 1060 that has been ordered
+-- get the total no of customer
+-- Create one view
